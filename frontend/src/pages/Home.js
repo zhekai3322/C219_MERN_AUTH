a433e289e7bbc2a2d65af14270b65b5ba4bebb47
zhekai3322 <23024477@myrp.edu.sg>
2025-02-12 15:05:37 +0800

    t2

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts', {
+      const response = await fetch('http://localhost:4000/api/workouts', {
         headers: {'Authorization': `Bearer ${user.token}`},
       })
       const json = await response.json()
