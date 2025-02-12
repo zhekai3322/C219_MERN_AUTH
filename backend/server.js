713024232896131dd964ec03cc55b5cfae8ee4a2
zhekai3322 <23024477@myrp.edu.sg>
2025-02-12 14:33:15 +0800

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+
+// express app
+const app = express()
+
+// middleware to handle CORS
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow requests from React frontend
+}))
+
+// middleware to parse JSON
+app.use(express.json())
+
+// log the requests
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
